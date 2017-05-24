48a76f684469baf1c488952118e17e96e0eecda0
Daniel <cppsgraphics@gmail.com>
2017-05-24 15:30:46 -0400

    Update aero.js

--- aero.js
+++ aero.js
@@ -1,15 +1,23 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = '-';
+const prefix = "$";
 
-client.on('ready', () => {
-  console.log('All systems functional!');
+client.on("ready", () => {
+  console.log("All systems functional!");
 });
 
-client.on('message', message => {
+client.on("message", message => {
+  if (message.author.bot) return;
+  let args = message.content.split(" ").slice(1);
 
-  if(message.content === (prefix +'ping')) {
-  message.channel.send('Ping?').then(m => m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms.`) );
+  if(message.content === (prefix + "ping")) {
+  message.channel.send("Ping?").then(m => m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms.`) );
+  }
+
+  if (message.content.startsWith(prefix + "add")) {
+      let numArray = args.map(n=> parseInt(n));
+      let total = numArray.reduce( (p, c) => p+c);
+      message.channel.send(total);
   }
 
 
@@ -19,33 +27,23 @@ client.on('message', message => {
         return message.channel.send("You are not authorized to use this command. :slight_frown:");
     }
     if(message.member.roles.has(modRole.id)) {
-    let args = message.content.split(" ").slice(1).join(" ");
     message.channel.bulkDelete(`${args}`)
 
     }
     }
 
-    if(message.content.startsWith(prefix + "status")) {
-    let args = message.content.split(" ").slice(1).join(" ");
-    client.user.setStatus(`${args}`)
+
+    if(message.content.startsWith(prefix + "setgame")) {
+      var argresult = args.join(" ");
+      client.user.setGame(argresult)
     }
 
 
-    if(message.content.startsWith(prefix + 'setgame')) {
-      let args = message.content.split(" ").slice(1);
-      var argresult = args.join(' ');
-      if (message.author.id !== "217411439948726272")   return;
-      client.user.setGame(argresult);
-    }
-
-
-    if(message.content.startsWith(prefix + 'reset')) {
+    if(message.content === (prefix + "reset")) {
       if (message.author.id !== "217411439948726272")   return;
       process.exit()
     }
 
-
-
 });
 
-client.login("insert token");
+client.login("your bot token here");
