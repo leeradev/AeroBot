f58e686b328c85ace5441dd0f6484d6bd1441073
Leera <leeradev@gmail.com>
2017-05-24 18:59:54 -0400

    I forgot to require config.json

--- aero.js
+++ aero.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = config.prefix
+const config = require('./config.json');
+const prefix = config.prefix;
 
 client.on("ready", () => {
   console.log("All systems functional!");
